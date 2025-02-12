5f8122500cae50b9a0e95da352284f13cfe67be4
Maria <voronovam3105@gmail.com>
2025-02-12 14:21:55 +0100

    Update one more

--- index.php
+++ index.php
@@ -7,19 +7,11 @@
     <link rel="stylesheet" href="css.css">
 </head>
 <body>
-    <h1>Hello</h1>
+    <h1>Hello world</h1>
     <nav class="main_navi">
 
 <?php
 
-if ($_GET['page'] == 1) {
-    echo "
-    <a class='active' href='index.php?page=1'>Home</a>
-    <a class='' href='index.php?page=2'>About me</a>
-    <a class='' href='index.php?page=3'>Shop</a>
-    ";
-    require_once 'page1.php';
-}
 ?>
 </nav>
 
